efc2261f6fef5d4a05097bbe79430172a26add86
Priyansh Jain <48293372+ErR0rpj@users.noreply.github.com>
2020-10-12 23:34:27 +0530

    Todoey App started

---
+++ todey/lib/screens/tasks_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 60, left: 30, right: 30, bottom: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Icon(
+                  Icons.list,
+                  size: 30,
+                ),
+                Text(
+                  'Todoey',
+                  style: TextStyle(fontSize: 50),
+                ),
+                Text(
+                  '12 Tasks',
+                  style: TextStyle(fontSize: 30),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
